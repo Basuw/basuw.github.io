e8bc9cc2cd0f25f805d4bca381480769e01014a6
bajacqueli <bastien.jacquelin@etu.uca.fr>
2023-06-29 16:46:17 +0200

    2 pages : home and cv empty nav bar working in view.js

---
+++ src/views/nav-bar.js
@@ -0,0 +1,46 @@
+export default {
+	data: function(){
+		return {
+			open:true,
+		}
+	},
+	methods: {
+		openClose:function () {
+			var img=document.querySelector("#imgNavbar");
+			open ? (open=false, navbar.setAttribute('style','display:none'), img.src='/icon/menu.svg') : (open=true, navbar.setAttribute('style','display:true'), img.src='/icon/menu_open.svg');
+		},
+		setStyle: function(page){
+			switch (page) {
+				case 'home':
+					document.querySelector("#home").setAttribute('class','active')
+					console.log(page, 'cici')
+					break;
+				case 'cv':
+					document.querySelector("#cv").setAttribute('class','active')
+					console.log(page)
+					break;
+			}
+		}
+	},
+	template: 
+		`
+		<table id="tabNav">
+		<tr>
+			<td>
+				<div id="navbar">
+					<a id="home" href="/index.html">Accueil</a>
+					<a id="cv" href="/src/pages/curriculum-vitae.html">CV</a>
+					<a @click="setStyle" id="home" href="#galerie">Galerie</a>
+					<a id="home" href="#contact">Contact</a>
+				</div>
+			</td>
+			<td>
+				<button id='openMenu' @click="openClose">
+					<img id='imgNavbar' src="/icon/menu_open.png">
+				</button>
+			</td>
+		</tr>
+	  </table>
+	  <span>{{page}}</span>
+		`
+}
