9d9d2cbb93b986782abdeb5f64dae0092680b008
bajacqueli <bastien.jacquelin@etu.uca.fr>
2023-12-19 22:53:11 +0100

    init.sql with all schemas

--- data/init.sql
+++ data/init.sql
@@ -1,11 +1,31 @@
 -- Crée une table
 CREATE TABLE Activity (
-    id integer PRIMARY KEY,
+    id INT PRIMARY KEY,
     name VARCHAR (100),
     description VARCHAR (100),
     unit VARCHAR (10)
+);
+CREATE TABLE User (
+    id INT PRIMARY KEY,
+    name VARCHAR (100),
+    pasword VARCHAR (100),
+);
+CREATE TABLE Achieve (
+    id INT PRIMARY KEY,
+    actId INT,
+    achievment INT,
+    dateAchieve date
+    FOREIGN KEY (actId) REFERENCES Activity(id)
+);
+CREATE TABLE Save (
+    id INT PRIMARY KEY,
+    actId INT,
+    frequence
+    FOREIGN KEY (actId) REFERENCES Activity(id)
+
 );
 
 -- Insère des données dans la table
 INSERT INTO Activity VALUES (0,'read','read description','pages');
 INSERT INTO Activity VALUES (1,'hiking','hiking description','km');
+INSERT INTO User VALUES (0,'dev','123');
