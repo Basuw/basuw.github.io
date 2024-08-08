f9710f2a1fe95d77f97083fa0ff7052a8e13350f
Bastien Jacquelin <bastien.jacquelin_ext@michelin.com>
2024-08-08 22:43:03 +0200

    Adding vue js to project, add dev tab, new fontsss ...

---
+++ views/development.js
@@ -0,0 +1,18 @@
+export default{
+	emits: [''],
+	data : function(){
+		return {
+		}
+	},
+	methods: {
+	},
+	template:
+	`
+	<div class="font-container">
+		<p class="font-arial">This is Aerial font.</p>
+		<p class="font-chillax">This is Chillax font.</p>
+		<p class="font-vercetti">This is Vercetti font.</p>
+		<p class="font-Nohemi">This is Nohemi font.</p>
+	</div>
+	`
+}
---
+++ views/home.js
@@ -0,0 +1,13 @@
+export default{
+	emits: [''],
+	data : function(){
+		return {
+		}
+	},
+	methods: {
+	},
+	template:
+	`
+		<p>Hi, I'm Bastien</p>
+	`
+}
