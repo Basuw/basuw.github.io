8ef8ed75bd35f50937f8ae42da420b9dadaa0dc8
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-09-03 22:21:27 +0200

    chore: Add contact page and styles

---
+++ views/contact.js
@@ -0,0 +1,32 @@
+export default{
+	emits: [''],
+	data : function(){
+		return {
+		}
+	},
+	methods: {
+	},
+	template:
+	`
+        <div class="contact">
+            <h2>Don't hesitate to contact me !😊</h2>
+            <ul class="contact-links">
+                <li>
+                    <a href="https://www.linkedin.com/in/bastien-jacquelin" target="_blank">
+                        <i class="fab fa-linkedin"></i> LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/Basuw" target="_blank">
+                        <i class="fab fa-github"></i> GitHub
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:bastien.jacquelin@free.fr">
+                        <i class="fas fa-envelope"></i> Email
+                    </a>
+                </li>
+            </ul>
+        </div>
+	`
+}
