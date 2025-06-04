cb9c460acc9f820a0d86db7057a30b903854dea0
Basuw <bastien.jacquelin@free.fr>
2025-06-04 11:59:44 +0200

    feat: implement dynamic loading of timeline activities from JSON data

--- views/about/timeline/timeline.js
+++ views/about/timeline/timeline.js
@@ -5,63 +5,38 @@ export default {
 		activityBloc
 	},
 	emits: [''],
+	created() {
+		this.loadActivities();
+	},
 	data : function(){
 		return {
+			activities: []
 		}
 	},
 	methods: {
+		async loadActivities() {
+			try {
+				const response = await fetch('/data/timeline.json');
+				const activitiesData = await response.json();
+				this.activities = activitiesData;
+			} catch (error) {
+				console.error('Error loading timeline activities:', error);
+			}
+		}
 	},
 	template:
 	`
 	<div id="time">
-		<activityBloc
-			:isRelevant="false"
-			iconName="fas fa-graduation-cap"
-			date="2021"
-			description="Obtention of baccalauréat with honors in Math and Physics."
-			position="left"
-		></activityBloc>
-		<div class="timeline-circle"></div>
-		<activityBloc
-			:isRelevant="true"
-			iconName="fas fa-keyboard"
-			date="2023"
-			description="Started working for a famous tire manufacturer as trainee devops"
-			position="right"
-		></activityBloc>
-		<div class="timeline-circle"></div>
-		<activityBloc
-			:isRelevant="false"
-			iconName="fas fa-laptop-code"
-			date="2023"
-			description="Obtention of DUT in Computer Science at IUT Informatique Aubiere."
-			position="left"
-		></activityBloc>
-		<div class="timeline-circle"></div>
-		<activityBloc
-			:isRelevant="true"
-			iconName="fas fa-fire-extinguisher"
-			date="2024"
-			description="Started my engagement as a volunteer firefighter."
-			position="right"
-		></activityBloc>
-		<div class="timeline-circle"></div>
-		<activityBloc
-			:isRelevant="true"
-			iconName="fas fa-chalkboard-teacher"
-			date="2024"
-			description="Started giving private Maths lesson."
-			position="left"
-		></activityBloc>
-		<div class="timeline-circle"></div>
-		<activityBloc
-			:isRelevant="true"
-			iconName="fas fa-user-graduate"
-			date="2025"
-			description="2nd year of engineering degree in Computer Science at ISIMA."
-			position="right"
-		></activityBloc>
-		<div class="timeline-circle"></div>
+		<template v-for="(activity, index) in activities" :key="index">
+			<activityBloc
+				:isRelevant="activity.isRelevant"
+				:iconName="activity.iconName"
+				:date="activity.date"
+				:description="activity.description"
+				:position="activity.position"
+			></activityBloc>
+			<div class="timeline-circle"></div>
+		</template>
 	</div>
 	`
 }
